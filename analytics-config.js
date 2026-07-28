dd8b394375751c966e4b3ed80cc28972286cbb5b
Ykmmz <yangmengze608@gmail.com>
2026-07-29 00:47:02 +0800

    feat: add disabled privacy analytics configuration

---
+++ analytics-config.js
@@ -0,0 +1,7 @@
+window.AI_ANALYTICS_CONFIG = Object.freeze({
+  provider: 'goatcounter',
+  enabled: false,
+  siteCode: '',
+  allowedHostnames: ['aisaichang.cn', 'www.aisaichang.cn'],
+  respectPrivacySignals: true,
+});
